fea5063c896433158167ed3273160425a7f6f045
Diogo Pereira <diogo.pereira.ee@gmail.com>
2024-01-12 18:07:14 +0000

    update mini_serv1.c

--- mini_serv1.c
+++ mini_serv1.c
@@ -4,18 +4,35 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <string.h>
+#include <strings.h>
+
 
 #include <stdio.h>
+
+typedef struct s_client
+{
+	int id;
+	int fd;
+	char *msg_in;
+	char *msg_out;
+} t_client;
 
 void print_error(char *str);
 int get_server_fd(char *port);
 int accept_connection(int server_fd);
 void handle_connection(int fd);
+void update_max_fd(int *max_fd, int fd);
+void receive_msg(t_client *client, int fd);
+int str_len(char *str);
 
 int main(int argc, char **argv)
 {
-	fd_set current_sockets, ready_sockets;
+	t_client clients[65536];
+	fd_set current_sockets, r_sockets, w_sockets;
 	int server_fd, client_fd;
+	int id = 0;
+	int max_fd = 0;
+	char *msg_send = 0;
 
 	if (argc != 2)
 		print_error("Wrong number of arguments\n");
@@ -24,30 +41,34 @@ int main(int argc, char **argv)
 	if (server_fd < 0)
 		print_error("Fatal error\n");
 
-	
+	update_max_fd(&max_fd, server_fd);
+	bzero(clients, sizeof(clients));
 	FD_ZERO(&current_sockets);
 	FD_SET(server_fd, &current_sockets);
 
 	while (1)
 	{
-		ready_sockets = current_sockets;
-		if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
+		r_sockets =  w_sockets = current_sockets;
+		if (select(max_fd+1, &r_sockets, &w_sockets, NULL, NULL) < 0)
 			print_error("Fatal error\n");
 		
-		for (int i=0; i < FD_SETSIZE; i++)
+		for (int i=0; i < max_fd+1; i++)
 		{
-			if (FD_ISSET(i, &ready_sockets))
+			if (FD_ISSET(i, &r_sockets))
 			{
 				if (i == server_fd)
 				{
 					client_fd = accept_connection(server_fd);
 					if (client_fd < 0)
 						print_error("Fatal error\n");
+					update_max_fd(&max_fd, client_fd);
 					FD_SET(client_fd, &current_sockets);
 				}
 				else
 				{
-					handle_connection(i);
+					//handle_connection(i);
+					//receive_msg(&clients[i], i);
+					printf("msg: %s\n", clients[i].msg_in);
 				}
 			}			
 		}
@@ -75,7 +96,7 @@ int get_server_fd(char *port)
 	if (server_fd < 0)
 		return (-1);
 	
-	bzero(&address, sizeof(address)); 
+	bzero(&address, sizeof(address));
 	address.sin_family = AF_INET; 
 	address.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
 	address.sin_port = htons(atoi(port));
@@ -108,13 +129,103 @@ int accept_connection(int server_fd)
 
 void handle_connection(int fd)
 {
-	char buffer[1000];
+	char buffer[2];
 	int n_read;
 
-	n_read = read(fd, buffer, 999);
+	n_read = read(fd, buffer, 1);
 	buffer[n_read] = '\0';
 
 	printf("%s", buffer);
 
 	write(fd, "resposta\n", 9);
+}
+
+void update_max_fd(int *max_fd, int fd)
+{
+	if (fd > *max_fd)
+		*max_fd = fd;
+}
+
+/*
+// verificar se a ligacao fecha
+void receive_msg(t_client *client, int fd)
+{
+	const int buff_size = 2;
+	char buff_read[buff_size];
+	char *buff; 
+	int read_bytes;
+	int total_size;
+
+	read_bytes = recv(fd, buff_read, buff_size-1, 0);
+	buff_read[read_bytes] = '\0';
+	total_size = str_len(client->msg_in) + read_bytes + 1;
+
+	printf("client->msg_in: %s\n", client->msg_in);
+	printf("total size: %i\n", total_size);
+
+	buff = malloc(total_size);
+
+	if (client->msg_in)
+	{
+		strcat(buff, client->msg_in);
+		free(client->msg_in);
+	}
+	strcat(buff, buff_read);
+	buff[total_size] = '\0';
+	client->msg_in = buff;
+}
+
+int str_len(char *str)
+{
+	if (!str)
+		return (0);
+	return (strlen(str));
+}
+*/
+
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int	i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
 }
